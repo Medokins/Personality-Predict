8644d539e7c472c4bd5abe58636094ca12303062
Medokins <nikodemszwast@gmail.com>
2022-03-17 15:35:04 +0100

    add 2 datasets plots

--- visualisaton.r
+++ visualisaton.r
@@ -1,13 +1,20 @@
-pacman::p_load(pacman, caret, lars, tidyverse, rio)
-library(dplyr)
+pacman::p_load(pacman, caret, lars, tidyverse, rio, cowplot)
 library(ggplot2)
 
-data <- import("Datasets/mbti_1.csv")
+data1 <- import("Datasets/MBTI 500.csv")
+categories1 <- (data1 %>% group_by(type) %>% summarize(count = n()))
 
-categories <- (data %>% group_by(type) %>% summarize(count = n()))
+data2 <- import("Datasets/mbti_1.csv")
+categories2 <- (data2 %>% group_by(type) %>% summarize(count = n()))
 
-ggplot(categories, aes(x = type, y = count)) +
+barplot1 <- ggplot(categories1, aes(x = type, y = count)) +
     geom_col() +
     geom_text(aes(label = count), vjust = -0.3)
+
+barplot2 <- ggplot(categories2, aes(x = type, y = count)) +
+    geom_col() +
+    geom_text(aes(label = count), vjust = -0.3)
+
+plot_grid(barplot1, barplot2, labels = "AUTO")
 
 p_unload(all)
