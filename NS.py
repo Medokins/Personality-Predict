44fcf830054be7ba583519c668ffe9ed287c32d2
Medokins <nikodemszwast@gmail.com>
2022-04-01 19:37:49 +0200

    add NS classification algorithm

--- NS.py
+++ NS.py
@@ -1,63 +1,41 @@
 import pandas as pd
 
-judging_dict = pd.read_csv("Datasets/judging_df.csv")
-perceiving_dict = pd.read_csv("Datasets/perceiving_df.csv")
+intuition_dict = pd.read_csv("Datasets/intuition_df.csv")
+sensing_dict = pd.read_csv("Datasets/sensing_df.csv")
 
-judging_dict.columns = ["word", "score"]
-perceiving_dict.columns = ["word", "score"]
+intuition_dict.columns = ["word", "score"]
+sensing_dict.columns = ["word", "score"]
 
-judging_words = judging_dict["word"].tolist()
-perceiving_words = perceiving_dict["word"].tolist()
+intuition_words = intuition_dict["word"].tolist()
+sensing_words = sensing_dict["word"].tolist()
 
-#Juding post
-post_0 = "know intj tool use interaction people excuse antisocial truly enlighten mastermind know would count pet peeze something time\
-        matter people either whether group people mall never see best friend sit outside conversation jsut listen want interject sit\
-        formulate say wait inject argument thought find fascinate sit watch people talk people fascinate sit class watch different\
-        people find intrigue dad intj u stand look like line safeway watch people home talk people like think military job people\
-        voluntarily go job important show deference endanger live glorify way civilian think pretty ignorant general think military\
-        necessary defense mechanism political tactic feel like u specifically invest much money could put money education whatnot\
-        though personally sound budget aernative really comment one way base two politician eye year ago come name somewhat important\
-        kinda role model nowadays pick keep score individual level mean little vary accord number condition day may score high others low sweat\
-        really good cast physiotherapist like fiberglass cast break arm whatever sometimes want take picture beast put someone arm sadly people"
+#Intuition post
+post_0 = "easily understandable hard topic thank effort problem example especially inxp react fast dodge quick"
 
-#Perceiving post
-post_1 = "good start say look make deep run year trade would improve team considerably expect resign franson next year plus keep mention\
-        phil introvert like miss leg something really bad tavares introvert case realize would start introduce tell u one fun fact human\
-        intelligence consider endless course would really complicate technical rig probably coworker super friendly ask time know intent\
-        good man hate ask totally stop ask dick essentially ask stop say hi try clarify thought sound like try control manner one say hello\
-        would potentially even bad answer usually force good instead depth analysis life would appropriate give question say someone close ask\
-        totally relatively open tend talk much people tell sometimes smart intelligent maybe way speak mannerism something give away seem like lot\
-        people meet resent like go way make people feel stupid assume think good somehow believe think probably happy could relate wish would stop\
-        pas judgement even know act make seem stupid think add clarification difference role polr function go use beebe model well socionics provide"
+#Sensing post
+post_1 = "like spring feel energetic season whatever reason also sit outside first thunderstorm relax op want"
 
-#Perceiving post
-post_2 = "fulfil cheat good situation pretend everythings great side note ton people use work google work le recognizable entity name alphabet\
-        umbrella anyway like waymo verily surprise prestige work big tech name steadily drop next year favor work shadow company ut also mind get\
-        stock option big company part compensation blame abstract potential possibility something earn right regret chase fancy tech career bay area never\
-        get big fat payday point seem follow logic without xyz long get giant compensation giant compensation uncertain reward forgo xyz certain loss obviously\
-        nature delay gratification key maximize delay gratification carefully weigh odds miss spend prime life something satisfy ura risky gamble incredibly rare\
-        weah necessarily say kind sound like might turn fine continue rent room save money least year market crash great critical outcome perceive necessary make\
-        lifestyle worth outcome measurably likely thing want see side next decade besides fat bank account amass via frugality lifestyle still worth bad happen\
-        want kid need big house expensive car want life long relationship someone willingness forgo luxury car big house bay area exactly profound evidence humble\
-        attainable desire get far define need stuff consider important let work relationship actively try meet people wait hop right person come along latter true urge revisit\
-        lifelong relationship one specific thing actually know want forgo act seek one abstract possibility good job good city good look strange make say nah good wait get serious"
+#Sensing post
+post_2 = "say abusive holy shit sound like si sound like even though relate lol might worth simply list gender"
 
-def evaluate(post, perceiving_words, judging_words, perceiving_df, judging_df):
-    perceiving_score = 0
-    judging_score = 0
+def evaluate(post, intuition_words, sensing_words, intuition_df, sensing_df):
+    intuition_score = 0
+    sensing_score = 0
     for word in post.split():
-        if word in perceiving_words:    
-            perceiving_score += perceiving_df.loc[perceiving_df["word"] == word]["score"].tolist()[0]
-        if word in judging_words:
-            judging_score += judging_df.loc[judging_df["word"] == word]["score"].tolist()[0]
+        if word in intuition_words:    
+            intuition_score += intuition_df.loc[intuition_df["word"] == word]["score"].tolist()[0]
+        if word in sensing_words:
+            sensing_score += sensing_df.loc[sensing_df["word"] == word]["score"].tolist()[0]
     
 
-    print("Perceiving: ", perceiving_score, end = "  |  ")
-    print("Judging: ", judging_score)
+    print("Intuition: ", intuition_score, end = "  |  ")
+    print("Sensing: ", sensing_score)
 
-    if perceiving_score > judging_score: return "P"
-    else: return "J"
+    if intuition_score > sensing_score: return "N"
+    else: return "S"
 
-print(evaluate(post_0, perceiving_words, judging_words, perceiving_dict, judging_dict)) #J
-print(evaluate(post_1, perceiving_words, judging_words, perceiving_dict, judging_dict)) #P
-print(evaluate(post_2, perceiving_words, judging_words, perceiving_dict, judging_dict)) #P  this one it gets wrong
+print(evaluate(post_0, intuition_words, sensing_words, intuition_dict, sensing_dict)) #N
+print(evaluate(post_1, intuition_words, sensing_words, intuition_dict, sensing_dict)) #S this one is wrong
+print(evaluate(post_2, intuition_words, sensing_words, intuition_dict, sensing_dict)) #S
+
+
