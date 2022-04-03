d7c81121946501fc6244850433234b6520cd3d38
Medokins <nikodemszwast@gmail.com>
2022-04-03 12:29:44 +0200

    add post filtering function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from evaluate_full import evaluate_full_personality
+from modifyData import prepare_post
 
 post = "" #Paste Your post in here
+post = prepare_post(post)
 
 if len(post) == 0: print("You didn't paste in Your post!")
 else:
